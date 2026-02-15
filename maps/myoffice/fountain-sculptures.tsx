28db611822ce60b70dae82c3e65cc6ecd67ca787
Reaper404-wag <141778743+Reaper404-wag@users.noreply.github.com>
2026-02-15 21:15:42 +0300

    fountain future + inside polytech

---
+++ maps/myoffice/fountain-sculptures.tsx
@@ -0,0 +1,186 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="fountain-sculptures" tilewidth="32" tileheight="32" tilecount="800" columns="20">
+ <image source="fountain-sculptures.png" width="640" height="1280"/>
+ <tile id="122">
+  <animation>
+   <frame tileid="122" duration="200"/>
+   <frame tileid="126" duration="200"/>
+   <frame tileid="130" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="123">
+  <animation>
+   <frame tileid="123" duration="200"/>
+   <frame tileid="127" duration="200"/>
+   <frame tileid="131" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="142">
+  <animation>
+   <frame tileid="142" duration="200"/>
+   <frame tileid="146" duration="200"/>
+   <frame tileid="150" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="143">
+  <animation>
+   <frame tileid="143" duration="200"/>
+   <frame tileid="147" duration="200"/>
+   <frame tileid="151" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="162">
+  <animation>
+   <frame tileid="162" duration="200"/>
+   <frame tileid="166" duration="200"/>
+   <frame tileid="170" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="163">
+  <animation>
+   <frame tileid="163" duration="200"/>
+   <frame tileid="167" duration="200"/>
+   <frame tileid="171" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="182">
+  <animation>
+   <frame tileid="182" duration="200"/>
+   <frame tileid="186" duration="200"/>
+   <frame tileid="190" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="183">
+  <animation>
+   <frame tileid="183" duration="200"/>
+   <frame tileid="187" duration="200"/>
+   <frame tileid="191" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="201">
+  <animation>
+   <frame tileid="201" duration="200"/>
+   <frame tileid="205" duration="200"/>
+   <frame tileid="209" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="202">
+  <animation>
+   <frame tileid="202" duration="200"/>
+   <frame tileid="206" duration="200"/>
+   <frame tileid="210" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="203">
+  <animation>
+   <frame tileid="203" duration="200"/>
+   <frame tileid="207" duration="200"/>
+   <frame tileid="211" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="204">
+  <animation>
+   <frame tileid="212" duration="200"/>
+   <frame tileid="208" duration="200"/>
+   <frame tileid="204" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="222">
+  <animation>
+   <frame tileid="222" duration="200"/>
+   <frame tileid="226" duration="200"/>
+   <frame tileid="230" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="223">
+  <animation>
+   <frame tileid="223" duration="200"/>
+   <frame tileid="227" duration="200"/>
+   <frame tileid="231" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="250">
+  <animation>
+   <frame tileid="250" duration="200"/>
+   <frame tileid="246" duration="200"/>
+   <frame tileid="242" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="270">
+  <animation>
+   <frame tileid="270" duration="200"/>
+   <frame tileid="266" duration="200"/>
+   <frame tileid="262" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="290">
+  <animation>
+   <frame tileid="290" duration="200"/>
+   <frame tileid="286" duration="200"/>
+   <frame tileid="282" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="309">
+  <animation>
+   <frame tileid="309" duration="200"/>
+   <frame tileid="305" duration="200"/>
+   <frame tileid="301" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="310">
+  <animation>
+   <frame tileid="310" duration="200"/>
+   <frame tileid="306" duration="200"/>
+   <frame tileid="302" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="311">
+  <animation>
+   <frame tileid="311" duration="200"/>
+   <frame tileid="307" duration="200"/>
+   <frame tileid="303" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="329">
+  <animation>
+   <frame tileid="329" duration="200"/>
+   <frame tileid="325" duration="200"/>
+   <frame tileid="321" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="330">
+  <animation>
+   <frame tileid="330" duration="200"/>
+   <frame tileid="326" duration="200"/>
+   <frame tileid="322" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="331">
+  <animation>
+   <frame tileid="331" duration="200"/>
+   <frame tileid="327" duration="200"/>
+   <frame tileid="323" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="349">
+  <animation>
+   <frame tileid="349" duration="200"/>
+   <frame tileid="345" duration="200"/>
+   <frame tileid="341" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="350">
+  <animation>
+   <frame tileid="350" duration="200"/>
+   <frame tileid="346" duration="200"/>
+   <frame tileid="342" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="351">
+  <animation>
+   <frame tileid="351" duration="200"/>
+   <frame tileid="347" duration="200"/>
+   <frame tileid="343" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
